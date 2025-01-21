5625b1056cb06957406d14332428bb0e294535be
mariuszmatyasik <mariusz.matyasik@microsoft.wsei.edu.pl>
2025-01-22 00:05:48 +0100

    Add routes

---
+++ src/routes.tsx
@@ -0,0 +1,50 @@
+import {createBrowserRouter} from "react-router-dom";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Profile from "./pages/profile";
+import Home from "./pages/home";
+import CreatePost from "./pages/post";
+import Myphotos from "./pages/myphotos";
+import Error from "./pages/error";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+
+export const router = createBrowserRouter([
+    {
+        element: <ProtectedRoutes />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+                errorElement: <Error />
+            }
+        ],
+    },
+
+    {
+        path: '/post',
+        element: <CreatePost />,
+        errorElement: <Error />
+    },
+    {
+        path: '/profile',
+        element: <Profile />,
+        errorElement: <Error />
+    },
+    {
+        path: '/myphotos',
+        element: <Myphotos />,
+        errorElement: <Error />
+    },
+    {
+        path: '/login',
+        element: <Login />,
+        errorElement: <Error />
+    },
+    {
+        path: '/signup',
+        element: <Signup />,
+        errorElement: <Error />
+    }
+])
+
+export default router
