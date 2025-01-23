946382bc293666dee31f7a4da933279a056bd8ab
mariuszmatyasik <mariusz.matyasik@microsoft.wsei.edu.pl>
2025-01-23 01:51:49 +0100

    Add post layout and handle

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -12,7 +12,7 @@ const Layout: React.FC<ILayoutProps> = ({children}) => {
       <aside className="flex gap-x-4 bg-gray-800 fixed top-0 left-0 z-40 lg:w-60 h-screen">
           <Sidebar />
       </aside>
-      <div className="lg:ml-60 lg:mr-60 p-8 flex-q ml-36">{children}</div>
+      <div className="lg:ml-60 lg:mr-60 p-8 flex-1 ml-36">{children}</div>
       <aside className="hidden lg:block bg-gray-800 fixed top-0 right-0 z-40 lg:w-60 h-screen">
           <UserList />
       </aside>
--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,13 +1,66 @@
 import React from "react"
 import Layout from "@/components/layout";
+import {Label} from "@/components/ui/label.tsx";
+import {Textarea} from "@/components/ui/textarea.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import FileUploader from "@/components/fileUploader";
+import {useUserAuth} from "@/context/userAuthContext.tsx";
+import {FileEntry, Post} from "@/types";
 
 interface ICreatePostProps {
 }
 
 const CreatePost: React.FunctionComponent<ICreatePostProps> = () => {
-  return (
+    const { user } =useUserAuth();
+    const [fileEntry, setFileEntry] = React.useState<FileEntry>({
+        files: [],
+    });
+    const [post, setPost] = React.useState<Post>({
+        caption: "",
+        photos: [],
+        likes: 0,
+        userlikes: [],
+        userId: null,
+        date: new Date(),
+    })
+
+    const handleSubmit = async(e: React.MouseEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("Uploaded File Entry: ", fileEntry);
+        console.log("Create Post: ", post);
+    }
+
+    return (
     <Layout>
-      <div>Create post</div>
+      <div className="flex justify-center w-full">
+          <div className="border max-w-3xl w-full">
+              <h3 className="bg-slate-800 text-white text-center text-lg p-2">
+                  Create Post
+              </h3>
+              <div className="p-8">
+                  <form onSubmit={handleSubmit}>
+                      <div className="flex flex-col">
+                          <Label className="mb-4" htmlFor="caption">
+                              Photo Caption
+                          </Label>
+                          <Textarea className="mb-8"
+                                    id="caption"
+                                    placeholder="enter description"
+                          value={post.caption}
+                          onChange={(e) => setPost({...post, caption: e.target.value})}
+                          />
+                          <div className="flex flex-col">
+                              <Label className="mb-4" htmlFor="photo">
+                                  Photos
+                              </Label>
+                              <FileUploader fileEntry={fileEntry} onChange={setFileEntry} />
+                          </div>
+                          <Button className="mt-8 w-32" type="submit">Post</Button>
+                      </div>
+                  </form>
+              </div>
+          </div>
+      </div>
     </Layout>
   )
 };
