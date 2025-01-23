80ea3cb8f9ff7d5ed5be9d280a270f5151d9000a
Jarosław Łyczak <jaroslaw.lyczak@microsoft.wsei.edu.pl>
2025-01-23 19:01:14 +0100

    uploader

--- src/components/fileUploader/index.tsx
+++ src/components/fileUploader/index.tsx
@@ -1,30 +1,99 @@
-import React, {useRef, useState} from "react"
-import * as UC from '@uploadcare/file-uploader'
-import {FileUploaderRegular, OutputFileEntry} from '@uploadcare/react-uploader';
+import { useState, useRef, useEffect, useCallback } from "react";
+import * as LR from "@uploadcare/blocks";
+import { OutputFileEntry } from "@uploadcare/blocks";
+import { FileUploaderRegular } from '@uploadcare/react-uploader';
 import '@uploadcare/react-uploader/core.css';
-import {FileEntry} from "@/types";
+import { FileEntry } from "@/types";
 
-UC.defineComponents(UC);
+
 
 interface IFileUploaderProps {
-    fileEntry: FileEntry;
-    onChange: (fileentry: FileEntry) => void;
+  fileEntry: FileEntry;
+  onChange: (fileEntry: FileEntry) => void;
 }
 
-const FileUploader: React.FC<IFileUploaderProps> = ({fileEntry, onChange}) => {
-    const [uploadedFiles, setUploadedFiles] = useState<OutputFileEntry[]>([]);
-    const ctxProviderRef = useRef<InstanceType<UC.UploadCtxProvider>>(null);
-    const configRef = useRef<InstanceType<UC.Config>>(null);
+const FileUploader: React.FunctionComponent<IFileUploaderProps> = ({
+  fileEntry,
+  onChange,
+}) => {
+  const [uploadedFiles, setUploadedFiles] = useState<OutputFileEntry[]>([]);
+  const ctxProviderRef = useRef<
+    typeof LR.UploadCtxProvider.prototype & LR.UploadCtxProvider
+  >(null);
 
-    return (
-      <div>
-          <FileUploaderRegular
-              sourceList="local, url, camera"
-              classNameUploader="uc-light"
-              pubkey="9fc51b146131c222dec4"
-          />
+  const handleRemoveClick = useCallback(
+    (uuid: OutputFileEntry["uuid"]) =>
+      onChange({ files: fileEntry.files.filter((f) => f.uuid !== uuid) }),
+    [fileEntry.files, onChange]
+  );
+
+  useEffect(() => {
+    const handleUploadEvent = (e: CustomEvent<OutputFileEntry[]>) => {
+      if (e.detail) {
+        console.log("The uploaded file event is ; ", e);
+        setUploadedFiles([...e.detail]);
+      }
+    };
+    ctxProviderRef.current?.addEventListener("data-output", handleUploadEvent);
+    return () => {
+      ctxProviderRef.current?.removeEventListener(
+        "data-output",
+        handleUploadEvent
+      );
+    };
+  }, [setUploadedFiles]);
+
+  useEffect(() => {
+    const resetUploaderState = () =>
+      ctxProviderRef.current?.uploadCollection.clearAll();
+
+    const handleDoneFlow = () => {
+      resetUploaderState();
+
+      onChange({ files: [...uploadedFiles] });
+      setUploadedFiles([]);
+    };
+
+    ctxProviderRef.current?.addEventListener("done-flow", handleDoneFlow);
+
+    return () => {
+      ctxProviderRef.current?.removeEventListener("done-flow", handleDoneFlow);
+    };
+  }, [fileEntry, onChange, uploadedFiles, setUploadedFiles]);
+
+  return (
+    <div>
+     <FileUploaderRegular
+         sourceList="local, url, camera, dropbox"
+         classNameUploader="uc-light"
+         pubkey="f7a7afe1c093cb1fd188"
+      />
+
+      <lr-upload-ctx-provider ctx-name="my-uploader" ref={ctxProviderRef} />
+
+      <div className="grid grid-cols-2 gap-4 mt-8">
+        {fileEntry.files.map((file) => (
+          <div key={file.uuid} className="relative">
+            <img
+              key={file.uuid}
+              src={`${file.cdnUrl}/-/format/webp/-/quality/smart/-/stretch/fill/
+              `}
+            />
+
+            <div className="cursor-pointer flex justify-center absolute -right-2 -top-2 bg-white border-2 border-slate-800  rounded-full w-7 h-7">
+              <button
+                className="text-slate-800 text-center"
+                type="button"
+                onClick={() => handleRemoveClick(file.uuid)}
+              >
+                ×
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
-  )
-  }
+    </div>
+  );
+};
 
-  export default FileUploader;
+export default FileUploader;
