69e32a887379d2543e85551287fd424494ecb472
Jarosław Łyczak <jaroslaw.lyczak@microsoft.wsei.edu.pl>
2025-01-24 19:41:17 +0100

    postcard and other changes

--- src/repository/post.service.ts
+++ src/repository/post.service.ts
@@ -1,10 +1,11 @@
 import { db } from "@/firebaseConfig";
-import { Post } from "@/types";
+import { DocumentResponse, Post } from "@/types";
 import {
   addDoc,
   collection,
   deleteDoc,
   doc,
+  updateDoc,
   getDoc,
   getDocs,
   orderBy,
@@ -18,9 +19,27 @@ export const createPost = (post: Post) => {
   return addDoc(collection(db, COLLECTION_NAME), post);
 };
 
-export const getPosts = () => {
+export const getPosts = async () => {
+  try{
   const q = query(collection(db, COLLECTION_NAME), orderBy("date", "desc"));
-  return getDocs(q);
+  const querySnapshot = await getDocs(q);
+  const tempArr: DocumentResponse[] = [];  
+  if (querySnapshot.size > 0) {
+    querySnapshot.forEach((doc) => {
+      const data = doc.data() as Post;
+      const responseObj:DocumentResponse = {
+        id: doc.id,
+        ...data,
+      };
+      tempArr.push(responseObj) 
+    });
+    return tempArr;
+  } else {
+    console.log("No such document");
+  }
+} catch (error)  {
+    console.log(error);
+  }
 };
 
 export const getPostByUserId = (id: string) => {
@@ -35,4 +54,16 @@ export const getPost = (id: string) => {
 
 export const deletePost = (id: string) => {
   return deleteDoc(doc(db, COLLECTION_NAME, id));
+};
+
+export const updateLikesOnPost = (
+  id: string,
+  userlikes: string[],
+  likes: number
+) => { 
+  const docRef = doc(db, COLLECTION_NAME, id);
+  return updateDoc(docRef, { 
+    likes: likes,
+    userlikes: userlikes,
+   });
 };
